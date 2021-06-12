1ecf4dc2a9fe33a26142da28f49cbddd8a546523
Shubidumdu <skaty4870@naver.com>
2021-06-12 22:32:36 +0900

    Add media query

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import "normalize.css";
+import "./index.css";
+
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const pixelRatio = window.devicePixelRatio;
+  const width = (canvas.clientWidth * pixelRatio) | 0;
+  const height = (canvas.clientHeight * pixelRatio) | 0;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
 
 const canvas = document.querySelector("#game-of-life-canvas");
-const renderer = new THREE.WebGLRenderer({canvas});
+const renderer = new THREE.WebGLRenderer({ canvas });
 
 const fov = 75;
-const aspect = 2; 
+const aspect = 2;
 const near = 0.1;
 const far = 5;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
@@ -17,7 +31,7 @@ const boxWidth = 1;
 const boxHeight = 1;
 const boxDepth = 1;
 const geometry = new THREE.PlaneGeometry(boxWidth, boxHeight, boxDepth);
-const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
+const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
 const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
@@ -25,22 +39,27 @@ scene.add(cube);
 renderer.render(scene, camera);
 
 function render(time) {
-    time *= 0.001;  // convert time to seconds
-   
-    cube.rotation.x = time;
-    cube.rotation.y = time;
-   
-    renderer.render(scene, camera);
-   
-    requestAnimationFrame(render);
-  }
-  requestAnimationFrame(render);
+  time *= 0.001; // convert time to seconds
 
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  cube.rotation.x = time;
+  cube.rotation.y = time;
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
 
 {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
+  const color = 0xffffff;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
 }
